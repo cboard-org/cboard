f893f48fb076c8baa17acd1287174240743b3a40
Shay Cojocaru <shayc@outlook.com>
2017-08-24 22:20:16 +0300

    fix: used Paper and set max-width

--- src/components/FullScreenDialog/FullScreenDialog.js
+++ src/components/FullScreenDialog/FullScreenDialog.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
 import Button from 'material-ui/Button';
 import Dialog, { DialogContent } from 'material-ui/Dialog';
-import AppBar from 'material-ui/AppBar';
+import Paper from 'material-ui/Paper';
 import Toolbar from 'material-ui/Toolbar';
-import IconButton from 'material-ui/IconButton';
 import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
 import ArrowBackIcon from 'material-ui-icons/ArrowBack';
 import Slide from 'material-ui/transitions/Slide';
 
@@ -21,8 +22,16 @@ const styles = {
   title: {
     flex: 1
   },
-  content: {
+  container: {
+    height: '100%',
+    background: '#f1f1f1',
+    overflowY: 'auto',
     WebkitOverflowScrolling: 'touch'
+  },
+  paper: {
+    background: '#fff',
+    maxWidth: '680px',
+    margin: '0 auto'
   }
 };
 
@@ -30,47 +39,49 @@ function FullScreenDialog(props) {
   const { open, title, onCancel, onSubmit, classes, children } = props;
 
   return (
-    <div>
-      <Dialog
-        fullScreen
-        open={open}
-        transition={<Slide direction="up" />}
-        onRequestClose={() => {
-          onCancel();
-        }}
-      >
-        <AppBar className={classes.appBar}>
-          <Toolbar>
-            <div className="back-button">
-              <IconButton
-                color="contrast"
-                onClick={() => {
-                  onCancel();
-                }}
-              >
-                <ArrowBackIcon />
-              </IconButton>
-            </div>
-            <Typography type="title" color="inherit" className={classes.title}>
-              {title}
-            </Typography>
-            {onSubmit &&
-              <Button
-                color="contrast"
-                onClick={() => {
-                  onSubmit();
-                  onCancel();
-                }}
-              >
-                <FormattedMessage {...messages.save} />
-              </Button>}
-          </Toolbar>
-        </AppBar>
-        <DialogContent className={classes.content}>
-          {children}
-        </DialogContent>
-      </Dialog>
-    </div>
+    <Dialog
+      fullScreen
+      open={open}
+      transition={<Slide direction="up" />}
+      onRequestClose={() => {
+        onCancel();
+      }}
+    >
+      <AppBar className={classes.appBar}>
+        <Toolbar>
+          <div className="back-button">
+            <IconButton
+              color="contrast"
+              onClick={() => {
+                onCancel();
+              }}
+            >
+              <ArrowBackIcon />
+            </IconButton>
+          </div>
+          <Typography type="title" color="inherit" className={classes.title}>
+            {title}
+          </Typography>
+          {onSubmit &&
+            <Button
+              color="contrast"
+              onClick={() => {
+                onSubmit();
+                onCancel();
+              }}
+            >
+              <FormattedMessage {...messages.save} />
+            </Button>}
+        </Toolbar>
+      </AppBar>
+      <div className={classes.container}>
+        <Paper className={classes.paper} elevation={4}>
+          <DialogContent>
+            {children}
+          </DialogContent>
+        </Paper>
+      </div>
+    </Dialog>
   );
 }
 
