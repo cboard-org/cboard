42f348399fedf0c7b5fddaf6bc7afe7d31fb0b2d
Brendan Moore <BrendanFDMoore@users.noreply.github.com>
2017-09-01 11:06:02 -0400

    Tx. ARIA labels intl for speech settings (#60)
    * Add new terms from CrowdIn
    
    * Add new terms for aria-labels for buttons.

--- src/containers/Settings/Speech/messages.js
+++ src/containers/Settings/Speech/messages.js
@@ -1,17 +1,21 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  speech: {
-    id: 'cboard.containers.Settings.Speech.speech',
-    defaultMessage: 'Speech'
+  higher: {
+    id: 'cboard.containers.Settings.Speech.higher',
+    defaultMessage: 'Higher'
   },
-  sampleSentence: {
-    id: 'cboard.containers.Settings.Speech.sampleSentence',
-    defaultMessage: 'Hi! this is my voice'
+  lower: {
+    id: 'cboard.containers.Settings.Speech.lower',
+    defaultMessage: 'Lower'
   },
-  voice: {
-    id: 'cboard.containers.Settings.Speech.voice',
-    defaultMessage: 'Voice'
+  faster: {
+    id: 'cboard.containers.Settings.Speech.faster',
+    defaultMessage: 'Faster'
+  },
+  slower: {
+    id: 'cboard.containers.Settings.Speech.slower',
+    defaultMessage: 'Slower'
   },
   pitch: {
     id: 'cboard.containers.Settings.Speech.pitch',
@@ -19,7 +23,7 @@ export default defineMessages({
   },
   pitchDescription: {
     id: 'cboard.containers.Settings.Speech.pitchDescription',
-    defaultMessage: 'Raise or lower the pitch for the voice'
+    defaultMessage: 'Make the voice use a higher or lower pitch'
   },
   rate: {
     id: 'cboard.containers.Settings.Speech.rate',
@@ -28,5 +32,17 @@ export default defineMessages({
   rateDescription: {
     id: 'cboard.containers.Settings.Speech.rateDescription',
     defaultMessage: 'Make the voice speak faster or slower'
+  },
+  speech: {
+    id: 'cboard.containers.Settings.Speech.speech',
+    defaultMessage: 'Speech'
+  },
+  sampleSentence: {
+    id: 'cboard.containers.Settings.Speech.sampleSentence',
+    defaultMessage: 'Hi! This is my voice.'
+  },
+  voice: {
+    id: 'cboard.containers.Settings.Speech.voice',
+    defaultMessage: 'Voice'
   }
 });
