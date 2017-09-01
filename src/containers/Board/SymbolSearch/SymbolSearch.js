72b9ababaf99bd7780f2f2abd7c8c457365326b2
Amberley Romo <amberleyjohanna@gmail.com>
2017-08-31 22:37:33 -0500

    Refactor of SymbolSearch component

--- src/containers/Board/SymbolSearch/SymbolSearch.js
+++ src/containers/Board/SymbolSearch/SymbolSearch.js
@@ -9,95 +9,41 @@ import messages from './messages';
 import mulberrySymbols from '../../../api/mulberry-symbols.json';
 import './SymbolSearch.css';
 
-let symbols = [];
-
-function renderInput(inputProps) {
-  const { home, value, ref, ...other } = inputProps;
-
-  return (
-    <TextField
-      fullWidth
-      autoFocus={home}
-      value={value}
-      inputRef={ref}
-      InputProps={{
-        ...other
-      }}
-    />
-  );
-}
-
-function renderSuggestion(suggestion, { query, isHighlighted }) {
-  return (
-    <div
-      className={classNames(
-        { 'SymbolSearch__Suggestion--highlighted': isHighlighted },
-        'SymbolSearch__Suggestion'
-      )}
-    >
-      <img
-        className="SymbolSearch__Suggestion-img"
-        src={suggestion.src}
-        alt=""
-      />
-      <div>
-        {suggestion.id}
-      </div>
-    </div>
-  );
-}
-
-function renderSuggestionsContainer(options) {
-  const { containerProps, children } = options;
-
-  return (
-    <div {...containerProps}>
-      {children}
-    </div>
-  );
-}
-
-function getSuggestionValue(suggestion) {
-  return suggestion.id;
-}
-
-function getSuggestions(value) {
-  const inputValue = value.trim().toLowerCase();
-  const inputLength = inputValue.length;
-  let count = 0;
-
-  return symbols.filter(symbol => {
-    if (count >= 16) {
-      return false;
-    }
-
-    let keep = symbol.id.slice(0, inputLength) === inputValue;
-
-    if (!keep) {
-      const words = symbol.id.split(' ');
-      for (let i = 1; i < words.length; i += 1) {
-        keep = words[i].slice(0, inputLength) === inputValue;
-        if (keep) {
-          break;
-        }
-      }
-    }
-
-    if (keep) {
-      count += 1;
-    }
-    return keep;
-  });
-}
-
 export class SymbolSearch extends Component {
   state = {
     value: '',
-    suggestions: []
+    suggestions: [],
+    symbols: []
   };
 
+  render() {
+    const { intl } = this.props;
+    return (
+      <Autosuggest
+        renderInputComponent={this.renderInput}
+        suggestions={this.state.suggestions}
+        onSuggestionsFetchRequested={this.handleSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.handleSuggestionsClearRequested}
+        onSuggestionSelected={this.handleSuggestionSelected}
+        renderSuggestionsContainer={this.renderSuggestionsContainer}
+        getSuggestionValue={this.getSuggestionValue}
+        renderSuggestion={this.renderSuggestion}
+        highlightFirstSuggestion={true}
+        inputProps={{
+          autoFocus: true,
+          placeholder: intl.formatMessage(messages.searchAnImage),
+          label: intl.formatMessage(messages.searchAnImage),
+          value: this.state.value,
+          onChange: this.handleChange,
+        }}
+      />
+    );
+  }
+
   componentDidMount() {
-    symbols = this.translateSymbols(mulberrySymbols);
+    this.setState({
+      symbols: this.translateSymbols(mulberrySymbols)
+    });
   }
 
   translateSymbols(symbols) {
@@ -111,9 +57,90 @@ export class SymbolSearch extends Component {
     });
   }
 
+  renderInput(inputProps) {
+    const { home, value, label, ref, ...other } = inputProps;
+
+    return (
+      <TextField
+        fullWidth
+        autoFocus={home}
+        value={value}
+        label={label}
+        inputRef={ref}
+        InputProps={{
+          ...other
+        }}
+      />
+    );
+  }
+
+  renderSuggestion(suggestion, { query, isHighlighted }) {
+    return (
+      <div
+        className={classNames(
+          { 'SymbolSearch__Suggestion--highlighted': isHighlighted },
+          'SymbolSearch__Suggestion'
+        )}
+      >
+        <img
+          className="SymbolSearch__Suggestion-img"
+          src={suggestion.src}
+          alt=""
+        />
+        <div>
+          {suggestion.id}
+        </div>
+      </div>
+    );
+  }
+
+  renderSuggestionsContainer(options) {
+    const { containerProps, children } = options;
+
+    return (
+      <div {...containerProps}>
+        {children}
+      </div>
+    );
+  }
+
+  getSuggestionValue(suggestion) {
+    return suggestion.id;
+  }
+
+  getSuggestions(value) {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+    let count = 0;
+
+    let symbols = [...this.state.symbols];
+    return symbols.filter(symbol => {
+      if (count >= 16) {
+        return false;
+      }
+
+      let keep = symbol.id.slice(0, inputLength) === inputValue;
+
+      if (!keep) {
+        const words = symbol.id.split(' ');
+        for (let i = 1; i < words.length; i += 1) {
+          keep = words[i].slice(0, inputLength) === inputValue;
+          if (keep) {
+            break;
+          }
+        }
+      }
+
+      if (keep) {
+        count += 1;
+      }
+      return keep;
+    });
+  }
+
   handleSuggestionsFetchRequested = ({ value }) => {
     this.setState({
-      suggestions: getSuggestions(value)
+      suggestions: this.getSuggestions(value)
     });
   };
 
@@ -138,30 +165,6 @@ export class SymbolSearch extends Component {
       value: newValue
     });
   };
-
-  render() {
-    const { intl } = this.props;
-
-    return (
-      <Autosuggest
-        renderInputComponent={renderInput}
-        suggestions={this.state.suggestions}
-        onSuggestionsFetchRequested={this.handleSuggestionsFetchRequested}
-        onSuggestionsClearRequested={this.handleSuggestionsClearRequested}
-        onSuggestionSelected={this.handleSuggestionSelected}
-        renderSuggestionsContainer={renderSuggestionsContainer}
-        getSuggestionValue={getSuggestionValue}
-        renderSuggestion={renderSuggestion}
-        highlightFirstSuggestion={true}
-        inputProps={{
-          autoFocus: true,
-          placeholder: intl.formatMessage(messages.searchAnImage),
-          value: this.state.value,
-          onChange: this.handleChange
-        }}
-      />
-    );
-  }
 }
 
 SymbolSearch.propTypes = {
