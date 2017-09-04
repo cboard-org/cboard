b51b2feeaa02bb124b4d251539029b23c0e18b44
Shay Cojocaru <shayc@outlook.com>
2017-09-04 03:05:57 +0300

    fix: forgot to remove dep

--- src/containers/Board/EditToolbar/EditToolbar.js
+++ src/containers/Board/EditToolbar/EditToolbar.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
 import classNames from 'classnames';
