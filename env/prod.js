223896f5d0b2d6824f9dda156990d4493da2eaa5
Brendan Moore <brendan.moore@rangle.io>
2017-09-07 02:53:39 -0400

    Finish encrypt/decrypt for private config vars.

---
+++ env/prod.js
@@ -0,0 +1,11 @@
+/* 
+ * PROD CONFIG
+ * This is intended for deployment to a hosting service.
+ */
+
+const APP_URL = 'https://cboard.somehost.com';
+
+module.exports = {
+  APP_URL,
+  EXAMPLE_CONFIG: 'Prod config',
+};
