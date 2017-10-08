bbeacdfa11ad9623966c790c5de5e14aaf69bf14
Shay Cojocaru <shayc@outlook.com>
2017-10-08 22:46:12 +0300

    refactor: Symbol componet - comments and tests

--- src/components/Board/Symbol/Symbol.test.js
+++ src/components/Board/Symbol/Symbol.test.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Symbol } from './Symbol';
-
-const intl = { formatMessage: () => {} };
-const classes = {};
+import Symbol from './Symbol';
 
 it('renders without crashing', () => {
-  shallow(<Symbol int={intl} classes={classes} />);
+  shallow(<Symbol />);
+});
+
+it('renders with image', () => {
+  const img = 'path/to/img.svg';
+  const wrapper = shallow(<Symbol img={img} />);
+  expect(wrapper.find('.Symbol__image')).toHaveLength(1);
+});
+
+it('renders with correct image source path', () => {
+  const img = 'path/to/img.svg';
+  const wrapper = shallow(<Symbol img={img} />);
+  const symbolImage = wrapper.find('.Symbol__image');
+  expect(symbolImage.prop('src')).toEqual(img);
+});
+
+it('renders with label', () => {
+  const wrapper = shallow(<Symbol label="dummy label" />);
+  expect(wrapper.find('.Symbol__label')).toHaveLength(1);
 });
--- src/components/Board/SymbolDetails/SymbolDetails.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { SymbolDetails } from './SymbolDetails';
-
-const intl = { formatMessage: () => {} };
-
-it('renders without crashing', () => {
-  shallow(<SymbolDetails intl={intl} />);
-});
