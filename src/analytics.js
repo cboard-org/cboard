0676f4e3e467f5cf4c609f7605a3c0d08abfba78
Shay Cojocaru <shayc@outlook.com>
2017-10-21 01:21:01 +0300

    feat: analytics added to speech actions

--- src/analytics.js
+++ src/analytics.js
@@ -1,11 +1,13 @@
 import { createMiddleware } from 'redux-beacon';
 import { GoogleAnalytics } from 'redux-beacon/targets/google-analytics';
 import boardEventsMap from './components/Board/Board.analytics';
+import speechEventsMap from './speech/analytics';
 
 const eventsMap = {
-  ...boardEventsMap
+  ...boardEventsMap,
+  ...speechEventsMap
 };
-
+debugger;
 const googleAnalytics = createMiddleware(eventsMap, GoogleAnalytics);
 
 export default googleAnalytics;
