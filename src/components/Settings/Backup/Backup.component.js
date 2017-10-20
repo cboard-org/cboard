46cc6632badbe9c2460addf3196f6de9c15c0f6c
Shay Cojocaru <shayc@outlook.com>
2017-10-20 12:26:10 +0300

    style: removed unused import

--- src/components/Settings/Backup/Backup.component.js
+++ src/components/Settings/Backup/Backup.component.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import Button from 'material-ui/Button';
