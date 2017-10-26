dab7077b6b99c25f025327eec705aa2e191cec1e
Shay Cojocaru <shayc@outlook.com>
2017-10-27 02:05:59 +0300

    removed old speech

--- src/speech/constants.js
+++
@@ -1,6 +0,0 @@
-export const REQUEST_VOICES = 'cboard/speech/REQUEST_VOICES';
-export const RECEIVE_VOICES = 'cboard/speech/RECEIVE_VOICES';
-export const CHANGE_VOICE = 'cboard/Speech/CHANGE_VOICE';
-export const CHANGE_PITCH = 'cboard/Speech/CHANGE_PITCH';
-export const CHANGE_RATE = 'cboard/Speech/CHANGE_RATE';
-export const CHANGE_VOLUME = 'cboard/Speech/CHANGE_VOLUME';
