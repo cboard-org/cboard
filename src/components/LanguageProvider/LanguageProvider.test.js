123be6a5a3b15fe688ea57a5134dcde45013c267
Shay Cojocaru <shayc@outlook.com>
2017-11-16 21:09:58 +0200

    fix: test

--- src/components/LanguageProvider/LanguageProvider.test.js
+++ src/components/LanguageProvider/LanguageProvider.test.js
@@ -1,11 +1,16 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import { LanguageProvider } from './LanguageProvider';
+import React from 'react';
+import { shallow } from 'enzyme';
+import { LanguageProvider } from './LanguageProvider.container';
 
-// it('renders without crashing', () => {
-//   shallow(
-//     <LanguageProvider lang={'en-US'}>
-//       <div />
-//     </LanguageProvider>
-//   );
-// });
+it('renders without crashing', () => {
+  const props = {
+    lang: '',
+    setLangs: () => {},
+    changeLang: () => {}
+  };
+  shallow(
+    <LanguageProvider {...props}>
+      <div />
+    </LanguageProvider>
+  );
+});
