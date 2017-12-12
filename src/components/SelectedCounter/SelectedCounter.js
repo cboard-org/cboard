3f3b3a53f51f529a8b7475a6b8e72cadd4ed0998
Arthur Denner <arthurdenner7@gmail.com>
2017-12-12 18:32:12 -0300

    refactor(components): Refactoring propTypes declaration for SelectedCounter

--- src/components/SelectedCounter/SelectedCounter.js
+++ src/components/SelectedCounter/SelectedCounter.js
@@ -2,20 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './SelectedCounter.css';
 
+const propTypes = {
+  count: PropTypes.number,
+  text: PropTypes.string
+};
+
+const defaultProps = {
+  count: 0,
+  text: 'items selected'
+};
+
 const SelectedCounter = ({ count, text }) => (
   <div className="SelectedCounter">
     <span>{`${count} ${text}`}</span>
   </div>
 );
 
-SelectedCounter.propTypes = {
-  count: PropTypes.number,
-  text: PropTypes.string
-};
-
-SelectedCounter.defaultProps = {
-  count: 0,
-  text: 'items selected'
-};
+SelectedCounter.propTypes = propTypes;
+SelectedCounter.defaultProps = defaultProps;
 
 export default SelectedCounter;
