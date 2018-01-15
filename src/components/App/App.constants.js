f100aac33275f8cbaf30aa6b8a20d78dc6832976
Arthur Denner <arthurdenner7@gmail.com>
2018-01-15 10:52:28 -0300

    refactor(welcome-screen): Fixing action type naming

--- src/components/App/App.constants.js
+++ src/components/App/App.constants.js
@@ -1,5 +1,5 @@
 // action types constants
-export const FINISH_FIRST_VISIT = 'cboard/Speech/FINISH_FIRST_VISIT';
+export const FINISH_FIRST_VISIT = 'cboard/App/FINISH_FIRST_VISIT';
 
 // language constants
 export const DEFAULT_LANG = 'en-US';
