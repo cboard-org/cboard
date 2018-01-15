5360d7aa05a529e186c3edd7f9772f3ecffdb004
Arthur Denner <arthurdenner7@gmail.com>
2018-01-15 14:35:54 -0300

    feat(welcome-screen): Adding PropTypes to new components

--- src/components/FormItems/RadioGroup.js
+++ src/components/FormItems/RadioGroup.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { RadioGroup as MUIRadioGroup } from 'material-ui/Radio';
 import { FormControl, FormLabel } from 'material-ui/Form';
 
-const RadioGroup = ({ children, className, label, error, ...rest }) => (
+const propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
+  className: PropTypes.string,
+  error: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  label: PropTypes.string
+};
+
+const defaultProps = {
+  className: '',
+  error: false,
+  label: ''
+};
+
+const RadioGroup = ({ children, className, error, label, ...rest }) => (
   <FormControl className={className} error={!!error}>
     <FormLabel>{label}</FormLabel>
     <MUIRadioGroup {...rest}>{children}</MUIRadioGroup>
   </FormControl>
 );
+
+RadioGroup.propTypes = propTypes;
+RadioGroup.defaultProps = defaultProps;
 
 export default RadioGroup;
--- src/components/FormItems/TextField.js
+++ src/components/FormItems/TextField.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MUITextField from 'material-ui/TextField';
 import { FormControl, FormHelperText } from 'material-ui/Form';
+
+const propTypes = {
+  className: PropTypes.string,
+  error: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.string,
+    PropTypes.number
+  ])
+};
+
+const defaultProps = {
+  className: '',
+  error: false
+};
 
 const TextField = ({ className, error, ...props }) => (
   <FormControl className={className} error={!!error}>
@@ -8,5 +23,8 @@ const TextField = ({ className, error, ...props }) => (
     {error && <FormHelperText>{error}</FormHelperText>}
   </FormControl>
 );
+
+TextField.propTypes = propTypes;
+TextField.defaultProps = defaultProps;
 
 export default TextField;
