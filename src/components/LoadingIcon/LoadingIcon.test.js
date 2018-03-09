6d488e0576ebd359500765154971debc4a027a92
Chaim Lando <chiptus@gmail.com>
2018-03-09 16:27:19 +0200

    add loading-icon snapshot test

---
+++ src/components/LoadingIcon/LoadingIcon.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallowMatchSnapshot } from '../../common/test_utils';
+import LoadingIcon from './LoadingIcon';
+
+describe('LoadingIcon tests', () => {
+  test('default renderer', () => {
+    shallowMatchSnapshot(<LoadingIcon />);
+  });
+});
