6e92e9af2770ad6d8d9479a77ad33b7e25e57964
Chaim Lando <chiptus@gmail.com>
2018-03-09 16:31:17 +0200

    symbol search snapshot

---
+++ src/components/SymbolSearch/SymbolSearch.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallowMatchSnapshot } from '../../common/test_utils';
+import SymbolSearch from './SymbolSearch';
+
+import { IntlProvider } from 'react-intl';
+
+describe('SymbolSearch tests', () => {
+  test('default renderer', () => {
+    shallowMatchSnapshot(
+      <IntlProvider locale="en">
+        <SymbolSearch />
+      </IntlProvider>
+    );
+  });
+});
