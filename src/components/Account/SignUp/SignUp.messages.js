fecd739323acbbec1f19acadbec74b9a3b4165a4
Shay Cojocaru <shayc@outlook.com>
2018-04-05 22:47:20 +0300

    Fix/console warnings (#185)
    * fix(console warning): material-ui dense
    
    * fix(console warning): missing translations
    
    * cleanup(signup): removed unused items

--- src/components/Account/SignUp/SignUp.messages.js
+++ src/components/Account/SignUp/SignUp.messages.js
@@ -9,21 +9,9 @@ export default defineMessages({
     id: 'cboard.components.SignUp.name',
     defaultMessage: 'Name'
   },
-  username: {
-    id: 'cboard.components.SignUp.username',
-    defaultMessage: 'Username'
-  },
   email: {
     id: 'cboard.components.SignUp.email',
     defaultMessage: 'Email'
-  },
-  age: {
-    id: 'cboard.components.SignUp.age',
-    defaultMessage: 'Age'
-  },
-  language: {
-    id: 'cboard.components.SignUp.language',
-    defaultMessage: 'Language'
   },
   createYourPassword: {
     id: 'cboard.components.SignUp.createYourPassword',
@@ -36,14 +24,6 @@ export default defineMessages({
   gender: {
     id: 'cboard.components.SignUp.gender',
     defaultMessage: 'Gender'
-  },
-  male: {
-    id: 'cboard.components.SignUp.male',
-    defaultMessage: 'Male'
-  },
-  female: {
-    id: 'cboard.components.SignUp.female',
-    defaultMessage: 'Female'
   },
   cancel: {
     id: 'cboard.components.SignUp.cancel',
