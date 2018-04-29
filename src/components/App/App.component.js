f01c7772ede7671989f2bc4b1504cbc8c6bda778
Shay Cojocaru <shayc@outlook.com>
2018-04-29 22:14:45 +0300

    refactor(App): removed wrapper, better seperation

--- src/components/App/App.component.js
+++ src/components/App/App.component.js
@@ -1,39 +1,64 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import { Route, Switch } from 'react-router-dom';
 
-import registerServiceWorker from '../../registerServiceWorker';
+import Activate from '../Account/Activate';
+import AuthScreen, { RedirectIfLogged } from '../AuthScreen';
+import BoardContainer from '../Board';
 import Notifications from '../Notifications';
-import BoardWrapper from '../Board';
+import NotFound from '../NotFound';
+import Settings from '../Settings';
+import WelcomeScreen from '../WelcomeScreen';
 import './App.css';
 
 export class App extends Component {
   static propTypes = {
     /**
-     * App language
-     */
-    lang: PropTypes.string.isRequired,
-    /**
      * App language direction
      */
-    dir: PropTypes.string.isRequired
+    dir: PropTypes.string.isRequired,
+    /**
+     * If 'true', user first visit
+     */
+    isFirstVisit: PropTypes.bool,
+    /**
+     * If 'true', user is logged in
+     */
+    isLogged: PropTypes.bool,
+    /**
+     * App language
+     */
+    lang: PropTypes.string.isRequired
   };
 
-  componentDidMount() {
-    const { onNewContentAvailable, onContentCached } = this.props;
-    registerServiceWorker(onNewContentAvailable, onContentCached);
-  }
-
   render() {
-    const { lang, dir } = this.props;
+    const { lang, dir, isFirstVisit, isLogged } = this.props;
 
     return (
       <div className="App">
         <Helmet>
           <html lang={lang} dir={dir} />
         </Helmet>
-        <BoardWrapper />
+
         <Notifications />
+
+        <Route
+          component={isFirstVisit && !isLogged ? WelcomeScreen : BoardContainer}
+          path="/"
+        />
+
+        <Switch>
+          <RedirectIfLogged
+            component={AuthScreen}
+            isLogged={isLogged}
+            path="/login-signup"
+            to="/"
+          />
+          <Route path="/settings" component={Settings} />
+          <Route path="/activate/:url" component={Activate} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
     );
   }
