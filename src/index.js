ab185e21680495ff641a9a30244373f652b61d88
Shay Cojocaru <shayc@outlook.com>
2018-10-12 23:14:31 +0300

    fix notification

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/es/integration/react';
 
-import * as serviceWorker from './serviceWorker';
 import configureStore, { getStore } from './store';
 import SpeechProvider from './providers/SpeechProvider';
 import LanguageProvider from './providers/LanguageProvider';
@@ -31,8 +30,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
