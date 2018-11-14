044cd8bcd00305af9e66822845c0baf5b3eaaadf
Natalia <gon4arovanatalija@gmail.com>
2018-11-14 19:25:59 +0200

    import constants to reducer

--- src/components/VoiceRecorder/VoiceRecorder.reducer.js
+++ src/components/VoiceRecorder/VoiceRecorder.reducer.js
@@ -1,13 +1,16 @@
+import { ADD_RECORD } from './VoiceRecorder.constants';
+import { START_RECORD } from './VoiceRecorder.constants';
+
 const initialState = { audioURL: '', iconsColor: 'black' };
 
 function voiceRecorderReducer(state = initialState, action) {
   switch (action.type) {
-    case 'ADD_RECORD':
+    case ADD_RECORD:
       return {
         audioURL: action.Blob,
         iconsColor: 'black'
       };
-    case 'START_RECORD':
+    case START_RECORD:
       return {
         iconsColor: action.color
       };
