f9348cd314989ea625d591dbdba5589529e557ed
Manuel Villafañe <manuel.villafane@engiefactory.com>
2019-03-26 23:42:07 -0300

    Add ARASAAC_BASE_PATH_API const

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 let host = window.location.host || '';
 host = host.startsWith('www.') ? host.slice(4) : host;
 const DEV_API_URL = process.env.REACT_APP_DEV_API_URL || null;
+export const ARASAAC_BASE_PATH_API = 'https://api.arasaac.org/api/';
 export const API_URL =
   DEV_API_URL || `${window.location.protocol}//api.${host}`;
