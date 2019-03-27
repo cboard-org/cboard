4dc448610811d64300995811dfc7befe6aa71326
Manuel Villafañe <manuel.villafane@engiefactory.com>
2019-03-26 23:41:50 -0300

    Add arasaacPictogramsSearch method

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { API_URL } from '../constants';
+import { API_URL, ARASAAC_BASE_PATH_API } from '../constants';
 import { getStore } from '../store';
 import { dataURLtoFile } from '../helpers';
 
@@ -35,8 +35,28 @@ class API {
   }
 
   async getLanguage(lang) {
-    const { data } = await this.axiosInstance.get(`/languages/${lang}`);
-    return data;
+    try {
+      const { status, data } = await this.axiosInstance.get(
+        `/languages/${lang}`
+      );
+      if (status === 200) return data;
+      return null;
+    } catch (err) {
+      return null;
+    }
+  }
+
+  async arasaacPictogramsSearch(locale, searchText) {
+    const pictogSearchTextPath = `${ARASAAC_BASE_PATH_API}pictograms/${locale}/search/${searchText}`;
+    try {
+      const { status, data } = await this.axiosInstance.get(
+        pictogSearchTextPath
+      );
+      if (status === 200) return data;
+      return [];
+    } catch (err) {
+      return [];
+    }
   }
 
   async login(email, password) {
