2076e6f6af7386b2a25a40ac7f570c4953513c3e
Shay Cojocaru <shayc@outlook.com>
2019-04-12 21:08:21 +0300

    add: blacklist

--- src/sw-template.js
+++ src/sw-template.js
@@ -11,7 +11,16 @@ if (workbox) {
   workbox.routing.registerNavigationRoute(
     // Assuming '/index.html' has been precached,
     // look up its corresponding cache key.
-    workbox.precaching.getCacheKeyForURL('/index.html')
+    workbox.precaching.getCacheKeyForURL('/index.html'),
+    {
+      blacklist: [
+        // Exclude URLs starting with /_, as they're likely an API call
+        new RegExp('^/_'),
+        // Exclude URLs containing a dot, as they're likely a resource in
+        // public/ and not a SPA route
+        new RegExp('/[^/]+\\.[^/]+$')
+      ]
+    }
   );
 
   workbox.routing.registerRoute(
