fa4e894b26f4b06829527b7f009b97ca71b3ffae
martin <martnbedouret@gmail.com>
2019-05-09 10:55:09 -0300

    Revert "Merge branch 'master' into push-api-redux"
    This reverts commit 80f2b921865b30aab58f108da486bf40da734ee8.

--- src/components/Board/Board.container.js
+++ src/components/Board/Board.container.js
@@ -162,10 +162,13 @@ export class BoardContainer extends Component {
       history
     } = this.props;
 
-    const { board, boards, communicator, changeBoard, addBoards } = this.props;
-    
-    //synchronize communicator and boards with API
-    this.props.getApiObjects();
+    const { board, boards, communicator, changeBoard, addBoards, userData } = this.props;
+
+    // Loggedin user?
+    if ('name' in userData && 'email' in userData) {
+      //synchronize communicator and boards with API
+      this.props.getApiObjects();
+    }
     
     if (!board || (id && board.id !== id)) {
       let boardId = id || communicator.rootBoard;
@@ -743,10 +746,3 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(injectIntl(BoardContainer));
-
-    const { board, boards, communicator, changeBoard, addBoards, userData } = this.props;
-    // Loggedin user?
-    if ('name' in userData && 'email' in userData) {
-      //synchronize communicator and boards with API
-      this.props.getApiObjects();
-    }
