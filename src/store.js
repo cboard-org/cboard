5114d172da23874546751a2038dc93c77f45bc27
martin <martnbedouret@gmail.com>
2019-05-10 14:19:30 -0300

    remove logger from master

--- src/store.js
+++ src/store.js
@@ -6,12 +6,11 @@ import { UPDATE_CONNECTIVITY } from './components/App/App.constants';
 import googleAnalytics from './analytics';
 import createReducer from './reducers';
 import buildVersion from './build-version';
-import { logger } from 'redux-logger'
 
 let store;
 
 export default function configureStore(initialState = {}) {
-  const middlewares = [thunk, logger, googleAnalytics];
+  const middlewares = [thunk, googleAnalytics];
   const enhancers = [applyMiddleware(...middlewares)];
   // If Redux DevTools Extension is installed use it, otherwise use Redux compose
   /* eslint-disable no-underscore-dangle */
