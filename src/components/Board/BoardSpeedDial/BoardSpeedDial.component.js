da3fe0f044b26e6753d4d6322e55a9feb0231106
martin bedouret <martinbedouret@gmail.com>
2019-05-21 11:11:54 -0300

    wip

--- src/components/Board/BoardSpeedDial/BoardSpeedDial.component.js
+++ src/components/Board/BoardSpeedDial/BoardSpeedDial.component.js
@@ -7,30 +7,22 @@ import { withStyles } from '@material-ui/core/styles';
 import SpeedDial from '@material-ui/lab/SpeedDial';
 import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon';
 import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
-import FileCopyIcon from '@material-ui/icons/FileCopyOutlined';
-import SaveIcon from '@material-ui/icons/Save';
+import AddPictogramIcon from '@material-ui/icons/AddPhotoAlternate';
+import AddBoardIcon from '@material-ui/icons/AddToPhotos';
 
 import messages from './BoardSpeedDial.messages';
 import './BoardSpeedDial.css';
 
-
-
-
 const styles = theme => ({
   root: {
-    height: 380
+    height: 38
   },
   speedDial: {
     position: 'absolute',
     bottom: theme.spacing.unit * 2,
-    right: theme.spacing.unit * 3
+    right: theme.spacing.unit * 4
   }
 });
-
-const actions = [
-  { icon: <FileCopyIcon />, name: 'Add new board', option: 'board' },
-  { icon: <SaveIcon />, name: 'Add new pictogram', option: 'pictogram' }
-];
 
 export class BoardSpeedDial extends Component {
 
@@ -60,7 +52,27 @@ export class BoardSpeedDial extends Component {
       open: false,
       hidden: false
     };
+
+    this.actions = this.getActions();
   }
+
+  getActions = () => {
+    const { intl } = this.props;
+    return [
+      {
+        icon: <AddBoardIcon />,
+        name: intl.formatMessage(messages.addNewBoard),
+        option: 'board',
+        delay: 1
+      },
+      {
+        icon: <AddPictogramIcon />,
+        name: intl.formatMessage(messages.addNewPictogram),
+        option: 'pictogram',
+        delay: 2
+      }
+    ];
+  };
 
   handleVisibility = () => {
     this.setState(state => ({
@@ -95,7 +107,6 @@ export class BoardSpeedDial extends Component {
 
     return (
       <div className={classes.root}>
-        <div className="mobile-only">
           <SpeedDial
             ariaLabel="SpeedDial tooltip example"
             className={classes.speedDial}
@@ -109,18 +120,18 @@ export class BoardSpeedDial extends Component {
             onMouseLeave={this.handleClose}
             open={open}
           >
-            {actions.map(action => (
+            {this.actions.map(action => (
               <SpeedDialAction
                 key={action.name}
                 icon={action.icon}
                 tooltipTitle={action.name}
                 tooltipOpen
                 onClick={onAddClick}
+                delay={action.delay}
               />
             ))}
           </SpeedDial>
         </div>
-      </div>
     );
   }
 }
--- src/components/Board/BoardSpeedDial/BoardSpeedDial.messages.js
+++ src/components/Board/BoardSpeedDial/BoardSpeedDial.messages.js
@@ -1,24 +1,12 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-  select: {
-    id: 'cboard.components.Board.BoardSpeedDial.select',
-    defaultMessage: 'Select'
+  addNewPictogram: {
+    id: 'cboard.components.Board.BoardSpeedDial.addNewPictogram',
+    defaultMessage: 'Add New Pictogram'
   },
-  cancel: {
-    id: 'cboard.components.Board.BoardSpeedDial.cancel',
-    defaultMessage: 'Cancel'
-  },
-  deleteTiles: {
-    id: 'cboard.components.Board.BoardSpeedDial.deleteTiles',
-    defaultMessage: 'Delete selected tiles'
-  },
-  editTiles: {
-    id: 'cboard.components.Board.BoardSpeedDial.editTiles',
-    defaultMessage: 'Edit selected tiles'
-  },
-  createTiles: {
-    id: 'cboard.components.Board.BoardSpeedDial.createTiles',
-    defaultMessage: 'Create tiles'
+  addNewBoard: {
+    id: 'cboard.components.Board.BoardSpeedDial.addNewBoard',
+    defaultMessage: 'Add New Board'
   }
 });
