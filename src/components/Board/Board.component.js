3550eda50c8dca13e4e01d213aeb65c3171b8880
Shay Cojocaru <shayc@outlook.com>
2019-06-09 13:41:34 +0300

    add: initial test

--- src/components/Board/Board.component.js
+++ src/components/Board/Board.component.js
@@ -72,6 +72,17 @@ export class Board extends Component {
     navHistory: PropTypes.arrayOf(PropTypes.string)
   };
 
+  static defaultProps = {
+    displaySettings: {
+      uiSize: 'Standard'
+    },
+    navigationSettings: {},
+    scannerSettings: { active: false, delay: 2000, strategy: 'automatic' },
+    selectedTileIds: [],
+
+    userData: {}
+  };
+
   constructor(props) {
     super(props);
 
@@ -82,6 +93,7 @@ export class Board extends Component {
   }
 
   componentDidMount() {
+    debugger;
     if (this.props.scannerSettings.active) {
       this.props.onScannerActive();
     }
