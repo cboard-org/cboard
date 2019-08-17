b41a20e121ebcbcf426c4910ea6a90bafa9a2bdc
martin bedouret <martinbedouret@gmail.com>
2019-08-17 16:44:38 -0300

    wip

--- src/cordova-util.js
+++ src/cordova-util.js
@@ -1,3 +1,4 @@
 export const isCordova = () => !!window.cordova;
 export const onCordovaReady = onReady =>
   document.addEventListener('deviceready', onReady, false);
+export var permissions = window.cordova.plugins.permissions;
