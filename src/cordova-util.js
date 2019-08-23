c9167f51f22dbb286d9fd4c3bbbcb6a9ed40fb79
martin bedouret <martinbedouret@gmail.com>
2019-08-23 16:51:20 -0300

    wip

--- src/cordova-util.js
+++ src/cordova-util.js
@@ -5,6 +5,50 @@ export const requestCvaPermissions = () => {
   if (isCordova()) {
     var permissions = window.cordova.plugins.permissions;
     permissions.checkPermission(
+      permissions.READ_EXTERNAL_STORAGE,
+      function(status) {
+        console.log('Has READ_EXTERNAL_STORAGE:', status.hasPermission);
+        if (!status.hasPermission) {
+          permissions.requestPermission(
+            permissions.READ_EXTERNAL_STORAGE,
+            function(status) {
+              console.log(
+                'success requesting READ_EXTERNAL_STORAGE permission'
+              );
+            },
+            function(err) {
+              console.warn('No permissions granted for READ_EXTERNAL_STORAGE');
+            }
+          );
+        }
+      },
+      function(err) {
+        console.log(err);
+      }
+    );
+
+    permissions.checkPermission(
+      permissions.RECORD_AUDIO,
+      function(status) {
+        console.log('Has RECORD_AUDIO:', status.hasPermission);
+        if (!status.hasPermission) {
+          permissions.requestPermission(
+            permissions.CAMERA,
+            function(status) {
+              console.log('success requesting RECORD_AUDIO permission');
+            },
+            function(err) {
+              console.warn('No permissions granted for RECORD_AUDIO');
+            }
+          );
+        }
+      },
+      function(err) {
+        console.log(err);
+      }
+    );
+
+    permissions.checkPermission(
       permissions.CAMERA,
       function(status) {
         console.log('Has CAMERA:', status.hasPermission);
@@ -19,20 +63,6 @@ export const requestCvaPermissions = () => {
             }
           );
         }
-        permissions.checkPermission(permissions.RECORD_AUDIO, function(status) {
-          console.log('Has RECORD_AUDIO:', status.hasPermission);
-          if (!status.hasPermission) {
-            permissions.requestPermission(
-              permissions.CAMERA,
-              function(status) {
-                console.log('success requesting RECORD_AUDIO permission');
-              },
-              function(err) {
-                console.warn('No permissions granted for RECORD_AUDIO');
-              }
-            );
-          }
-        });
       },
       function(err) {
         console.log(err);
