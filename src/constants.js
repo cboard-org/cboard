06c6e75c87ff60cfd6695ffb60e923a2b6c7420d
martin bedouret <martinbedouret@gmail.com>
2019-10-06 16:32:11 -0300

    set urls

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,9 @@
 let host = window.location.host || '';
 host = host.startsWith('www.') ? host.slice(4) : host;
-const DEV_API_URL = process.env.REACT_APP_DEV_API_URL || null;
+const DEV_API_URL = 'https://api.app.cboard.io';
 export const ARASAAC_BASE_PATH_API = 'https://api.arasaac.org/api/';
-export const API_URL = 'http://api.app.cboard.io';
+export const API_URL =
+  DEV_API_URL || `${window.location.protocol}//api.${host}`;
 export const TAWASOL_BASE_PATH_API =
   'https://madaportal.org/tawasol/wp-json/custom/v2/dea44ade76/';
 export const TAWASOL_BASE_IMAGE_ULR =
