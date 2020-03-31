e15c19c5fe1f1cab78045bfe08706313c68859ad
martin bedouret <martinbedouret@gmail.com>
2020-03-31 14:04:27 -0300

    wip

--- src/components/Settings/Display/Display.container.js
+++ src/components/Settings/Display/Display.container.js
@@ -7,16 +7,22 @@ import Display from './Display.component';
 import API from '../../../api';
 
 export class DisplayContainer extends PureComponent {
-  updateDisplaySettings = async (displaySettings) => {
+  updateDisplaySettings = async displaySettings => {
     try {
       await API.updateSettings({ display: displaySettings });
-    } catch (e) { }
+    } catch (e) {}
     this.props.updateDisplaySettingsAction(displaySettings);
   };
 
   render() {
     const { history } = this.props;
-    return <Display {...this.props} updateDisplaySettings={this.updateDisplaySettings} onClose={history.goBack} />;
+    return (
+      <Display
+        {...this.props}
+        updateDisplaySettings={this.updateDisplaySettings}
+        onClose={history.goBack}
+      />
+    );
   }
 }
 
