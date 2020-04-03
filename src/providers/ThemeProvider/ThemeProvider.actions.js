9e72e80a425d5eb3d3cca177509f19c950af066e
martin bedouret <martinbedouret@gmail.com>
2020-04-02 21:08:08 -0300

    removed

--- src/providers/ThemeProvider/ThemeProvider.actions.js
+++
@@ -1,8 +0,0 @@
-import { CHANGE_THEME } from './ThemeProvider.constants';
-
-export function changeTheme(theme) {
-  return {
-    type: CHANGE_THEME,
-    theme
-  };
-}
--- src/providers/ThemeProvider/ThemeProvider.constants.js
+++ src/providers/ThemeProvider/ThemeProvider.constants.js
@@ -1,3 +1,2 @@
-export const CHANGE_THEME = 'cboard/Speech/CHANGE_THEME';
 export const DEFAULT_THEME = 'light';
 export const DARK_THEME = 'dark';
