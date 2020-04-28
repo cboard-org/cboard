bfb9b9132c92f34aacdaf72893049f56ee9e8946
martin bedouret <martinbedouret@gmail.com>
2020-04-28 10:51:10 -0300

    fix

--- src/components/Settings/Speech/Speech.component.js
+++ src/components/Settings/Speech/Speech.component.js
@@ -30,6 +30,13 @@ const styles = theme => ({
   }
 });
 
+const getVoiceLabel = voice => {
+  if (voice.name === 'srpski Crna Gora') {
+    return voice.voiceURI;
+  }
+  return isCordova() ? voice.name + ' - ' + voice.voiceURI : voice.name;
+};
+
 const Speech = ({
   anchorEl,
   classes,
@@ -65,9 +72,7 @@ const Speech = ({
           >
             <ListItemText
               primary={<FormattedMessage {...messages.voice} />}
-              secondary={
-                isCordova() ? voice.name + ' - ' + voice.voiceURI : voice.name
-              }
+              secondary={getVoiceLabel(voice)}
             />
           </ListItem>
           <ListItem divider aria-label={intl.formatMessage(messages.pitch)}>
