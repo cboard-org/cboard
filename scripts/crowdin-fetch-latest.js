30e4edb5284ffc2bd966ba1e48a31306fc46b611
Shay Cojocaru <shayc@outlook.com>
2020-06-19 21:43:19 +0300

    Fix/translation crowdin fetch script (#718)
    * fix: add CBOARD_ENV variable
    
    * fix: filter cboard translation files
    
    * fix: translations
    
    * fix: rename Montenegrin file
    
    * fix: default values
    
    * update: translations
    
    * fix: translations
    
    * fix: translations
    
    * fix: sync with crowdin

--- scripts/crowdin-fetch-latest.js
+++ scripts/crowdin-fetch-latest.js
@@ -4,75 +4,84 @@ console.log('Pulling translations from CrowdIn.');
 
 const https = require('https');
 const fs = require('fs');
-const resolve = require('path').resolve
+const resolve = require('path').resolve;
 const DecompressZip = require('decompress-zip');
 
 const zipFilePath = resolve('./alltx.zip');
-const extractPath = resolve('./src/translations')
-const cboardSrcPath = resolve('./src/translations/src/cboard.json')
+const extractPath = resolve('./src/translations');
+const cboardSrcPath = resolve('./src/translations/src/cboard.json');
 
-const downloadCboardJson = (onComplete) => {
+const downloadCboardJson = onComplete => {
   console.log('Trying to download latest cboard.json...');
-  
+
   const cboardJson = fs.createWriteStream(cboardSrcPath);
-  https.get(`https://api.crowdin.com/api/project/cboard/export-file?file=cboard.json&language=en&key=${CROWDIN_API_KEY}`, function(response) {
-    response.pipe(cboardJson);
-    cboardJson.on('finish', function() {
-      console.log('cboard.json download complete.');
-      cboardJson.close(onComplete);
-    });
-    cboardJson.on('error', function(err) {
-      console.log('cboard.json download encountered error!');
-      console.log(err);
-    });
-  });
+  https.get(
+    `https://api.crowdin.com/api/project/cboard/export-file?file=cboard.json&language=en&key=${CROWDIN_API_KEY}`,
+    function(response) {
+      response.pipe(cboardJson);
+      cboardJson.on('finish', function() {
+        console.log('cboard.json download complete.');
+        cboardJson.close(onComplete);
+      });
+      cboardJson.on('error', function(err) {
+        console.log('cboard.json download encountered error!');
+        console.log(err);
+      });
+    }
+  );
 };
 
-const downloadTranslations = (onComplete) => {
+const downloadTranslations = onComplete => {
   console.log('Trying to download latest translation strings...');
-  
+
   const allTxZip = fs.createWriteStream(zipFilePath);
-  https.get(`https://api.crowdin.com/api/project/cboard/download/all.zip?key=${CROWDIN_API_KEY}`, function(response) {
-    response.pipe(allTxZip);
-    allTxZip.on('finish', function() {
-      console.log('Translation download complete.');
-      allTxZip.close(onComplete);
-    });
-    allTxZip.on('error', function(err) {
-      console.log('Translation download encountered error!');
-      console.log(err);
-    });
-  });
+  https.get(
+    `https://api.crowdin.com/api/project/cboard/download/all.zip?key=${CROWDIN_API_KEY}`,
+    function(response) {
+      response.pipe(allTxZip);
+      allTxZip.on('finish', function() {
+        console.log('Translation download complete.');
+        allTxZip.close(onComplete);
+      });
+      allTxZip.on('error', function(err) {
+        console.log('Translation download encountered error!');
+        console.log(err);
+      });
+    }
+  );
 };
 
 const deleteTemporaryDownloadFile = () => {
   console.log('Deleting temp file.');
-  fs.unlinkSync(zipFilePath)
+  fs.unlinkSync(zipFilePath);
 };
 
 const extractTranslations = () => {
   console.log('Extracting zip to translations folder.');
 
-  const unzipper = new DecompressZip(zipFilePath)
-    
-  unzipper.on('error', function (err) {
+  const unzipper = new DecompressZip(zipFilePath);
+
+  unzipper.on('error', function(err) {
     console.log('DecompressZip Caught an error:', err);
   });
-    
-  unzipper.on('extract', function (log) {
+
+  unzipper.on('extract', function(log) {
     console.log('DecompressZip finished extracting.');
     deleteTemporaryDownloadFile();
   });
-    
-  unzipper.on('progress', function (fileIndex, fileCount) {
+
+  unzipper.on('progress', function(fileIndex, fileCount) {
     console.log('Extracted file ' + (fileIndex + 1) + ' of ' + fileCount);
   });
-    
+
   unzipper.extract({
-      path: extractPath,
-      filter: function (file) {
-          return file.type !== "SymbolicLink";
-      }
+    path: extractPath,
+    filter: function(file) {
+      const isCboardFile =
+        file.type !== 'SymbolicLink' && !file.path.startsWith('website');
+
+      return isCboardFile;
+    }
   });
 };
 
