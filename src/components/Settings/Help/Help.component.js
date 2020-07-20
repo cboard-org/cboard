d8731ed318bd52f034ea78f2c75c793989818ec7
martin bedouret <martinbedouret@gmail.com>
2020-07-20 12:35:53 -0300

    wip

--- src/components/Settings/Help/Help.component.js
+++ src/components/Settings/Help/Help.component.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import ReactMarkdown from 'react-markdown';
@@ -8,30 +8,54 @@ import FullScreenDialog, {
 import messages from '../Settings.messages';
 import './Help.css';
 
-Help.propTypes = {
+const propTypes = {
   history: PropTypes.object.isRequired,
   onClose: PropTypes.func
 };
 
-function Help({ history, onClose }) {
-  const [markdown, setMarkdown] = useState(false);
-  const readmePath = require('../../../translations/help/en-US.md');
-  fetch(readmePath)
-    .then(response => {
-      return response.text();
-    })
-    .then(text => {
-      setMarkdown(text);
-    });
-  return (
-    <FullScreenDialog
-      open
-      title={<FormattedMessage {...messages.userHelp} />}
-      onClose={history.goBack}
-    >
-      <ReactMarkdown source={markdown} escapeHtml={false}/>
-    </FullScreenDialog>
-  );
+class Help extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      markdown: ''
+    };
+  }
+
+  componentDidMount() {
+    const readmePath = require('../../../translations/help/en-US.md');
+    fetch(readmePath)
+      .then(response => {
+        return response.text();
+      })
+      .then(text => {
+        this.setState({ markdown: text });
+      });
+  }
+
+  componentDidUpdate() {
+    let hash = window.location.hash.replace('#', '');
+    if (hash) {
+      let node = window.document.getElementsByName(hash)[0];
+      if (node) {
+        node.scrollIntoView();
+      }
+    }
+  }
+
+  render() {
+    return (
+      <FullScreenDialog
+        open
+        title={<FormattedMessage {...messages.userHelp} />}
+        onClose={this.props.history.goBack}
+      >
+        <ReactMarkdown source={this.state.markdown} escapeHtml={false} />
+      </FullScreenDialog>
+    );
+  }
 }
 
+Help.propTypes = propTypes;
 export default Help;
