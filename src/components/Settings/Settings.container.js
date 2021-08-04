2e35571a9d8a9050e367d35ecea10c9572103543
RodriSanchez1 <rodri.sanchez1998@gmail.com>
2021-08-04 11:34:53 -0300

    settings container conflict fixed

--- src/components/Settings/Settings.container.js
+++ src/components/Settings/Settings.container.js
@@ -5,17 +5,12 @@ import { PropTypes } from 'prop-types';
 import SettingsComponent from './Settings.component';
 import { logout } from '../Account/Login/Login.actions';
 import { getUser, isLogged } from '../App/App.selectors';
-import { setLangs } from '../../providers/LanguageProvider/LanguageProvider.actions';
-import { getVoices } from '../../providers/SpeechProvider/SpeechProvider.actions';
-import tts from '../../providers/SpeechProvider/tts';
-import { DEFAULT_LANG } from '../../components/App/App.constants';
-import { getVoicesLangs } from '../../i18n';
 import { injectIntl, intlShape } from 'react-intl';
 import { disableTour } from '../../components/App/App.actions';
 
 export class SettingsContainer extends Component {
   static propTypes = {
-    intl: intlShape.isRequired
+    intl: intlShape.isRequired,
     isLogged: PropTypes.bool.isRequired,
     user: PropTypes.object,
     logout: PropTypes.func.isRequired
