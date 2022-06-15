98856a243a43518654ae5b73d3509e6c53171306
tomivm <tomivm98@gmail.com>
2022-06-15 16:48:29 -0300

    wip

--- src/components/FixedGrid/Grid.js
+++ src/components/FixedGrid/Grid.js
@@ -41,89 +41,139 @@ function Grid(props) {
     return { right, left, up, down };
   };
 
+  const setFocusPositionFromFocusedButton = buttonElement => {
+    const setFocusPositionFromId = id => {
+      const divider = '-';
+      const positionArrayXY = id.split(divider);
+      const xPosition = parseInt(positionArrayXY[0]);
+      const yPosition = parseInt(positionArrayXY[1]);
+      focusPosition.x = xPosition;
+      focusPosition.y = yPosition;
+    };
+    if (buttonElement) {
+      const activeDraggableItem = buttonElement.parentNode;
+      const activeDroppableCellId = activeDraggableItem?.parentNode?.id;
+      if (activeDroppableCellId) setFocusPositionFromId(activeDroppableCellId);
+    }
+  };
+
   const handleOnKeyDown = event => {
     const keycode = event.code;
-    const { columns, rows } = other;
 
-    const setFocusPosition = pressedArrow => {
-      const { right, left, up, down } = pressedArrow;
-      const rightLimit = focusPosition.x >= columns - 1;
-      const leftLimit = focusPosition.x <= 0;
-      const topLimit = focusPosition.y <= 0;
-      const bottomLimit = focusPosition.y >= rows - 1;
-      if (right) {
-        if (rightLimit) {
-          focusPosition.x = 0;
+    const manageArrows = event => {
+      const setFocusPosition = pressedArrow => {
+        const { columns, rows } = other;
+        const { right, left, up, down } = pressedArrow;
+        const rightLimit = focusPosition.x >= columns - 1;
+        const leftLimit = focusPosition.x <= 0;
+        const topLimit = focusPosition.y <= 0;
+        const bottomLimit = focusPosition.y >= rows - 1;
+        if (right) {
+          if (rightLimit) {
+            focusPosition.x = 0;
+            return;
+          }
+          focusPosition.x = focusPosition.x + 1;
           return;
         }
-        focusPosition.x = focusPosition.x + 1;
-        return;
+        if (left) {
+          if (leftLimit) {
+            focusPosition.x = columns - 1;
+            return;
+          }
+          focusPosition.x = focusPosition.x - 1;
+          return;
+        }
+        if (up) {
+          if (topLimit) {
+            focusPosition.y = rows - 1;
+            return;
+          }
+          focusPosition.y = focusPosition.y - 1;
+          return;
+        }
+        if (down) {
+          if (bottomLimit) {
+            focusPosition.y = 0;
+            return;
+          }
+          focusPosition.y = focusPosition.y + 1;
+          return;
+        }
+      };
+
+      const pressedArrow = findPressedArrow(event);
+      if (!pressedArrow) return;
+      setFocusPosition(pressedArrow);
+
+      const currentId = `${focusPosition.x}-${focusPosition.y}`;
+      const currentTile = document.getElementById(currentId);
+
+      if (currentTile) {
+        const isAvailableTile = () => {
+          if (currentTile?.firstChild) {
+            currentTile.firstChild.querySelector('button').focus();
+            return true;
+          }
+          return false;
+        };
+
+        if (isAvailableTile()) return;
       }
-      if (left) {
-        if (leftLimit) {
-          focusPosition.x = columns - 1;
-          return;
-        }
-        focusPosition.x = focusPosition.x - 1;
-        return;
-      }
-      if (up) {
-        if (topLimit) {
-          focusPosition.y = rows - 1;
-          return;
-        }
-        focusPosition.y = focusPosition.y - 1;
-        return;
-      }
-      if (down) {
-        if (bottomLimit) {
-          focusPosition.y = 0;
-          return;
-        }
-        focusPosition.y = focusPosition.y + 1;
-        return;
+      manageArrows({
+        code: keycode,
+        preventDefault: event.preventDefault,
+        repeat: false
+      });
+    };
+
+    const manageTabs = () => {
+      const tabIsPressed = () => {
+        const tab = keycode === 'Tab';
+        if (tab) return true;
+        return false;
+      };
+      if (tabIsPressed()) {
+        const awaitTabSetNewFocus = () => {
+          const refreshFocusPosition = () => {
+            const activeButtonElement = document.activeElement;
+            setFocusPositionFromFocusedButton(activeButtonElement);
+          };
+          setTimeout(refreshFocusPosition, 0);
+        };
+        awaitTabSetNewFocus();
       }
     };
 
-    const pressedArrow = findPressedArrow(event);
-    if (!pressedArrow) return;
-    setFocusPosition(pressedArrow);
-
-    const currentId = `${focusPosition.x}-${focusPosition.y}`;
-    const currentTile = document.getElementById(currentId);
-
-    if (currentTile) {
-      const isAvailableTile = () => {
-        if (currentTile?.firstChild) {
-          currentTile.firstChild.querySelector('button').focus();
-          return true;
-        }
-        return false;
-      };
-
-      if (isAvailableTile()) return;
-    }
-    handleOnKeyDown({
-      code: keycode,
-      preventDefault: event.preventDefault,
-      repeat: false
-    });
+    manageArrows(event);
+    manageTabs();
   };
 
   useEffect(() => {
     const manageKeyDown = event => {
       if (findPressedArrow(event)) {
-        const focusFirstTile = () => {
-          const firstTile = document.getElementsByClassName('Tile')[0];
-          if (!firstTile) return;
-          firstTile.focus();
-          const firstTilePosition = firstTile.parentNode.parentNode.id;
-          focusPosition.x = parseInt(firstTilePosition[0]);
-          focusPosition.y = parseInt(firstTilePosition[2]);
+        const focusIsNotOnTile = () => {
+          const activeElement = document.activeElement;
+          const activeElementChildsArray = Array.from(
+            activeElement?.childNodes
+          );
+          if (
+            activeElementChildsArray.find(
+              element => element.className === 'Symbol'
+            )
+          )
+            return false;
+          return true;
         };
 
-        const activeElement = document.activeElement;
-        if (activeElement?.lastChild?.className !== 'Symbol') {
+        if (focusIsNotOnTile()) {
+          const focusFirstTile = () => {
+            const firstTile = document.getElementsByClassName('Tile')[0];
+            if (!firstTile) return;
+            firstTile.focus();
+            setFocusPositionFromFocusedButton(firstTile);
+          };
+
           focusFirstTile();
         }
       }
