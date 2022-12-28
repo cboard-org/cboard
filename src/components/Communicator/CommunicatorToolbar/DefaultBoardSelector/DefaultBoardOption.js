9731b47fc8a630e702c9b1dce1322404bdf3588d
Martin Bedouret <martinbedouret@gmail.com>
2022-12-28 12:01:29 -0300

    fix path for images in Cordova

--- src/components/Communicator/CommunicatorToolbar/DefaultBoardSelector/DefaultBoardOption.js
+++ src/components/Communicator/CommunicatorToolbar/DefaultBoardSelector/DefaultBoardOption.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { CardContent } from '@material-ui/core';
-
 import { Card, CardMedia, Typography } from '@material-ui/core';
 
+import { isCordova } from '../../../../cordova-util';
 import styles from './DefaultBoardSelector.module.css';
 
 import messages from '../CommunicatorToolbar.messages';
 
 const DefaultBoardOption = ({ rootBoard, onClick, intl }) => {
+  // Cordova path cannot be absolute
+  const image =
+    isCordova() && rootBoard.caption && rootBoard.caption.search('/') === 0
+      ? `.${rootBoard.caption}`
+      : rootBoard.caption;
+
   return (
     <Card className={styles.card} onClick={onClick}>
       <CardMedia
         component="img"
         alt={intl.formatMessage(messages.defaultBoardImageAlt)}
         height="140"
-        image={rootBoard.caption}
+        image={image}
       />
       <CardContent className={styles.cardContent}>
         <Typography gutterBottom variant="h6">
