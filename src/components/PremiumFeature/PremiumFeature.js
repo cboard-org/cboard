d0af48c100ddf9692523656f1f80c2aa5cca61a6
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-01-02 19:19:38 -0300

    Add subscription state to Premium feature

--- src/components/PremiumFeature/PremiumFeature.js
+++ src/components/PremiumFeature/PremiumFeature.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const captured = event => {
-  console.log('elem captured ');
-  event.stopPropagation();
-};
 function PremiumFeature(props) {
+  const captured = event => {
+    console.log('elem captured ');
+    if (!props.isSubscribed) {
+      event.stopPropagation();
+      console.log('TIENES QUE SUSCRIBIRTE');
+    }
+  };
+
   console.log(props.children);
   return <div onClickCapture={captured}>{props.children}</div>;
 }
 
-export default PremiumFeature;
+const mapStateToProps = state => ({
+  isSubscribed: state.subscription.isSubscribed
+});
+
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PremiumFeature);
