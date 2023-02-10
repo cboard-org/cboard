9becfd0e389bcff1b89d4f080faf65a9b7ec4bfe
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-02-09 22:26:02 -0300

    Rename refresh purchase function

--- src/providers/SubscriptionProvider/SubscriptionProvider.actions.js
+++ src/providers/SubscriptionProvider/SubscriptionProvider.actions.js
@@ -61,7 +61,7 @@ export function comprobeSubscription(payload) {
           : expiryDateFormat;
 
       if (isExpired) {
-        window.CdvPurchase.store.update();
+        window.CdvPurchase.store.restorePurchases();
 
         const isBillingRetryPeriodFinished = () => {
           console.log(
