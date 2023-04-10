9d82ad75a58de2ea11187775f0eb7f699fa8fb60
tomivm <tomivm98@gmail.com>
2023-04-10 17:05:11 -0300

    store stream from getUserMedia

--- src/components/VoiceRecorder/VoiceRecorder.component.js
+++ src/components/VoiceRecorder/VoiceRecorder.component.js
@@ -10,7 +10,7 @@ import IconButton from '../UI/IconButton';
 import { isCordova, requestCvaPermissions } from '../../cordova-util';
 import messages from './VoiceRecorder.messages';
 import './VoiceRecorder.css';
-
+let mediaStream = undefined;
 class VoiceRecorder extends Component {
   static propTypes = {
     /**
@@ -45,6 +45,7 @@ class VoiceRecorder extends Component {
         this.mediaRecorder = new window.MediaRecorder(stream);
         this.mediaRecorder.start();
         this.setState({ isRecording: true });
+        mediaStream = stream;
       })
       .catch(function(err) {
         console.log(err.message);
