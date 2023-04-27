aff4249f8eb8c89a4a55350c8b40485c546355c2
Martin Bedouret <martinbedouret@gmail.com>
2023-04-27 01:01:15 -0300

    fix

--- src/components/Settings/Subscribe/SubscriptionPlans.js
+++ src/components/Settings/Subscribe/SubscriptionPlans.js
@@ -5,7 +5,6 @@ import Box from '@material-ui/core/Box';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import { FormattedMessage } from 'react-intl';
 import { PayPalButtons } from '@paypal/react-paypal-js';
