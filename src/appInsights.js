3796a8fc2e44b57297661da1a98d78d37c222c10
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-06-28 19:18:52 -0300

    Create appInsights module and initilize it

---
+++ src/appInsights.js
@@ -0,0 +1,32 @@
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+import { NODE_ENV, AZURE_INST_KEY } from './constants';
+
+export const appInsights = new ApplicationInsights({
+  config: {
+    disableTelemetry: NODE_ENV === 'development',
+    instrumentationKey: AZURE_INST_KEY,
+    enableAutoRouteTracking: true,
+    loggingLevelTelemetry: 2,
+    enableCorsCorrelation: true,
+    enableRequestHeaderTracking: true,
+    enableResponseHeaderTracking: true,
+    enableAjaxErrorStatusText: true,
+    correlationHeaderExcludedDomains: [
+      '*.google-analytics.com',
+      'globalsymbols.com',
+      '*.arasaac.org',
+      'mulberrysymbols.org',
+      'madaportal.org',
+      '*.doubleclick.net',
+      'pagead2.googlesyndication.com',
+      'eastus.tts.speech.microsoft.com'
+    ]
+  }
+});
+
+const initializeAppInsights = () => {
+  appInsights.loadAppInsights();
+  appInsights.trackPageView();
+};
+
+export { initializeAppInsights };
