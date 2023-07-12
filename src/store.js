13e41eb1df3075e02814040f1fe37ac20d5de460
tomivm <tomivm98@gmail.com>
2023-07-12 19:29:49 -0300

    Allow Redux-devtools to test on qa
    Should be reverted

--- src/store.js
+++ src/store.js
@@ -14,9 +14,7 @@ export default function configureStore(initialState = {}) {
   // If Redux DevTools Extension is installed use it, otherwise use Redux compose
   /* eslint-disable no-underscore-dangle */
   const composeEnhancers =
-    process.env.NODE_ENV !== 'production' &&
-    typeof window === 'object' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
       ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
       : compose;
 
