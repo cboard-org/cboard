5135266aa6adcc9aede7e0095eb6f76aeba701d8
tomivm <tomivm98@gmail.com>
2023-07-21 21:03:02 -0300

    Add translations to Activate account screen

---
+++ src/components/Account/Activate/Activate.messages.js
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  startTour: {
+    id: 'cboard.components.Activate.startTour',
+    defaultMessage: 'Start a tour across Cboard'
+  },
+  loginSignUpPage: {
+    id: 'cboard.components.Activate.LoginSignUpPage',
+    defaultMessage: 'Login or sign up page'
+  }
+});
