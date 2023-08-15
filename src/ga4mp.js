162db16e06f1f45813f1a2f50bc9a8b2fa8f3a90
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-08-15 19:43:16 -0300

    Remove debug for GA4mp

--- src/ga4mp.js
+++ src/ga4mp.js
@@ -3,7 +3,7 @@ import { GA4_MEASUREMENT_ID } from './constants';
 
 const ga4track = ga4mp([GA4_MEASUREMENT_ID], {
   non_personalized_ads: true,
-  debug: true,
+  debug: false,
   user_agent:
     'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 });
