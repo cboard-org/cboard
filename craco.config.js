6b55f6bbe4d4227fa3b9a9832ab056ef9cc689e3
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-10-18 20:42:43 -0300

    Add craco for modify webpack config

---
+++ craco.config.js
@@ -0,0 +1,36 @@
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
+module.exports = {
+  webpack: {
+    plugins: [new NodePolyfillPlugin()],
+    module: {
+      loaders: [
+        {
+          test: /\.([cm]?ts|tsx)$/,
+          loaders: ['ts-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+      extensionAlias: {
+        '.js': ['.js', '.ts'],
+        '.cjs': ['.cjs', '.cts'],
+        '.mjs': ['.mjs', '.mts']
+      }
+    },
+
+    configure: {
+      ignoreWarnings: [
+        function ignoreSourcemapsloaderWarnings(warning) {
+          return (
+            warning.module &&
+            warning.module.resource.includes('node_modules') &&
+            warning.details &&
+            warning.details.includes('source-map-loader')
+          );
+        }
+      ]
+    }
+  }
+};
