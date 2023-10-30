54e9f5ac6f6c6ac57a14a1984f901e29939f8f43
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-10-30 16:30:41 -0300

    Optional chaining on ignoreWarning

--- craco.config.js
+++ craco.config.js
@@ -25,10 +25,8 @@ module.exports = {
       ignoreWarnings: [
         function ignoreSourcemapsloaderWarnings(warning) {
           return (
-            warning.module &&
-            warning.module.resource.includes('node_modules') &&
-            warning.details &&
-            warning.details.includes('source-map-loader')
+            warning.module?.resource.includes('node_modules') &&
+            warning.details?.includes('source-map-loader')
           );
         }
       ]
