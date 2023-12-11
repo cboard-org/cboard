5e3517a0627c6ce3b9841c4750d6bbb71eb3a80c
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-12-11 15:00:53 -0300

    Remove alias on craco.config

--- craco.config.js
+++ craco.config.js
@@ -1,19 +1,8 @@
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
-const path = require('path');
 
 module.exports = {
   webpack: {
     entry: './src/index.js',
-    alias: {
-      'react/jsx-dev-runtime': path.resolve(
-        __dirname,
-        'node_modules/react/jsx-dev-runtime.js'
-      ),
-      'react/jsx-runtime': path.resolve(
-        __dirname,
-        'node_modules/react/jsx-runtime.js'
-      )
-    },
     plugins: [new NodePolyfillPlugin({ excludeAliases: ['console'] })],
     resolve: {
       extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx']
