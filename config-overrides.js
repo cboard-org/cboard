ea3d9faffe79c8eb5f72b246d62fc2317888df98
jakubsimo <simak258@gmail.com>
2023-12-12 23:08:27 +0100

    Removing react-app-rewired package - Issue #1632
    Removing  react-app-rewired package - Issue #1632  - now only using craco instead, also updated part of README which was mentioning react-app-rewired

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,20 @@
+const webpack = require('webpack');
+
 module.exports = {
   webpack: function(config, env) {
+    // Added fallbacks
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        ...config.resolve.fallback,
+        crypto: require.resolve('crypto-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        path: require.resolve('path-browserify'),
+        stream: require.resolve('stream-browserify')
+      }
+    };
+
+    // Keeping existing overrides
     return {
       ...config,
       mode: 'development',
