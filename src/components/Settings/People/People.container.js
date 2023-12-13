f7e259f4e4c5a98f2588d414ea1e479298b9a9de
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-12-13 17:53:18 -0300

    window.FirebasePlugin.unregister() on user logout

--- src/components/Settings/People/People.container.js
+++ src/components/Settings/People/People.container.js
@@ -49,9 +49,7 @@ export class PeopleContainer extends PureComponent {
 
   handleLogout = () => {
     if (isAndroid()) {
-      window.plugins.googleplus.disconnect(function(msg) {
-        console.log('disconnect google msg' + msg);
-      });
+      window.FirebasePlugin.unregister();
       window.facebookConnectPlugin.logout(
         function(msg) {
           console.log('disconnect facebook msg' + msg);
