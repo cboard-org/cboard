f2f62bfda13b525f3bc8dcaf632d11e87071e6a7
Maxi Capodacqua <kapomaxi1@gmail.com>
2023-12-18 13:37:01 -0600

    Using anonymous instead

--- src/components/LoggedInFeature/LoginRequiredModal.messages.js
+++ src/components/LoggedInFeature/LoginRequiredModal.messages.js
@@ -7,8 +7,10 @@ export default defineMessages({
   },
   featureBlockedText: {
     id: 'cboard.components.LoginRequiredModal.featureBlockedText',
+    // Refer to un-logged in users as "anonymous users".
+    // https://english.stackexchange.com/questions/251800/a-word-for-a-non-logged-in-user
     defaultMessage:
-      'This feature is disabled for unlogged users. To continue using it please sign up or login'
+      'This feature is disabled for anonymous users. To continue using it please sign up or login'
   },
   loginSignupNow: {
     id: 'cboard.components.PremiumFeature.loginSignupNow',
