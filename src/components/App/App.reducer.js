033109a5b49c6f9b816bf771c9c75c4a37f693d7
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2024-07-08 16:52:51 -0300

    Use getKeysFromApiUserDataResponse function

--- src/components/App/App.reducer.js
+++ src/components/App/App.reducer.js
@@ -7,7 +7,8 @@ import {
   UPDATE_USER_DATA,
   DISABLE_TOUR,
   ENABLE_ALL_TOURS,
-  SET_UNLOGGED_USER_LOCATION
+  SET_UNLOGGED_USER_LOCATION,
+  USER_DATA_PROPERTIES
 } from './App.constants';
 import { LOGIN_SUCCESS, LOGOUT } from '../Account/Login/Login.constants';
 import {
@@ -58,6 +59,15 @@ const initialState = {
     arasaacActive: false
   },
   userData: {}
+};
+
+const getKeysFromApiUserDataResponse = payload => {
+  const newUser = {};
+  if (!payload) return newUser;
+  USER_DATA_PROPERTIES.forEach(prop => {
+    newUser[prop] = payload[prop];
+  });
+  return newUser;
 };
 
 function appReducer(state = initialState, action) {
@@ -147,7 +157,7 @@ function appReducer(state = initialState, action) {
         isFirstVisit: false,
         displaySettings,
         navigationSettings,
-        userData: action.payload || {}
+        userData: getKeysFromApiUserDataResponse(action.payload)
       };
     case LOGOUT:
       return {
@@ -155,10 +165,9 @@ function appReducer(state = initialState, action) {
         userData: {}
       };
     case UPDATE_USER_DATA:
-      if (action.userData.boards) delete action.userData.boards;
       return {
         ...state,
-        userData: action.userData
+        userData: getKeysFromApiUserDataResponse(action.userData)
       };
     case SET_UNLOGGED_USER_LOCATION:
       return {
