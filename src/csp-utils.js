d71fc89ac261e6f5d15678f6e38c10ca8dc786e6
Gonzalo Juarez <gonojuarez17@gmail.com>
2025-03-27 16:00:12 -0300

    refactor CSP links and setup for improved security and flexibility

--- src/csp-utils.js
+++ src/csp-utils.js
@@ -4,21 +4,32 @@ const localHostSources = isDevelopment
   : '';
 
 const cspLinks = {
-  localHost: localHostSources,
-  app: 'https://*.cboard.io ',
-  globalsymbols: 'https://globalsymbols.com/',
-  microsoft: 'wss://*.microsoft.com/cognitiveservices/',
+  app:
+    'https://*.cboard.io https://*.app.cboard.io/ https://*.app.qa.cboard.io/',
+  arasaac: 'https://api.arasaac.org/api/ https://*.arasaac.org/',
+  cboardDownload:
+    'https://cboardgroupqadiag.blob.core.windows.net/arasaac/arasaac.zip https://*.blob.core.windows.net/',
+  doubleclick: 'https://*.doubleclick.net https://stats.g.doubleclick.net/',
   eastus: 'eastus.tts.speech.microsoft.com',
-  arasaac: 'https://api.arasaac.org/api/',
+  facebook: 'https://*.facebook.com',
+  filePath: 'file://',
+  globalsymbols: 'https://globalsymbols.com/',
+  googleAnalytics:
+    'https://*.google-analytics.com https://www.google-analytics.com/',
+  googleLinks:
+    'https://*.google.com https://www.google.com/ads/ https://www.google.com.ar/ads/',
+  googleTagManager:
+    'https://*.googletagmanager.com https://www.googletagmanager.com/ http://www.googletagmanager.com/gtag/js',
+  localHost: `${localHostSources} http://localhost:3000/ http://localhost:10010/`,
+  microsoft:
+    'wss://*.microsoft.com/cognitiveservices/ https://*.microsoft.com/cognitiveservices/',
   paypal: 'https://*.paypal.com',
   paypalSandbox: 'https://*.sandbox.paypal.com',
-  googleLinks: 'https://*.google.com',
-  cboardDownload:
-    'https://cboardgroupqadiag.blob.core.windows.net/arasaac/arasaac.zip',
-  youtube: 'https://*.youtube.com',
-  doubleclick: 'https://*.doubleclick.net',
-  googleAnalytics: 'https://*.google-analytics.com',
-  googleTagManager: 'https://*.googletagmanager.com'
+  sslGstatic: 'https://*.gstatic.com',
+  vsTrack:
+    'https://dc.services.visualstudio.com/v2/track https://*.visualstudio.com/',
+  youtube: 'https://*.youtube.com https://www.youtube.com/',
+  cloudfront: 'https://*.cloudfront.net/'
 };
 
 const cspType = {
@@ -34,42 +45,70 @@ const cspType = {
 const setupCSP = () => {
   // Detectar entorno
 
-  const cstTypeAll = `${cspType.sell} ${cspType.unsafeInline} ${
+  const cstTypeAll = `${cspType.self} ${cspType.unsafeInline} ${
     cspType.unsafeEval
   }`;
 
-  const cspContent = `
-    default-src ${cstTypeAll} https://* ${cspLinks.app} ${
+  const defaultSrc = `default-src ${cstTypeAll} ${cspLinks.app} ${
     cspLinks.localHost
-  } blob: gap: data: ;
-    script-src ${cstTypeAll} ${cspLinks.app} ${cspLinks.paypal} ${
-    cspLinks.paypalSandbox
-  } ${cspLinks.localHost};
-    style-src 'self' 'unsafe-inline' ${cspLinks.app} ${cspLinks.paypal} ${
-    cspLinks.paypalSandbox
-  } ${cspLinks.localHost};
-    connect-src 'self' ${cspLinks.app} ${
+  } ${cspLinks.filePath} blob: gap: data:;`;
+
+  const scriptSrc = `script-src ${cstTypeAll} ${cspLinks.app} ${
+    cspLinks.paypal
+  } ${cspLinks.paypalSandbox} ${cspLinks.localHost} ${cspLinks.filePath} ${
+    cspLinks.cloudfront
+  };`;
+
+  const styleSrc = `style-src 'self' 'unsafe-inline' ${cspLinks.app} ${
+    cspLinks.paypal
+  } ${cspLinks.paypalSandbox} ${cspLinks.localHost} ${cspLinks.cloudfront};`;
+
+  const connectedSrc = `connect-src 'self' ${cspLinks.app} ${
     cspLinks.localHost
   } 'unsafe-inline' 'unsafe-eval' ${cspLinks.arasaac} ${cspLinks.globalsymbols} 
     ${cspLinks.microsoft} ${cspLinks.eastus} ${cspLinks.paypal} ${
     cspLinks.paypalSandbox
   } ${cspLinks.googleLinks} ${cspLinks.doubleclick} ${
     cspLinks.googleAnalytics
-  } ${cspLinks.googleTagManager} ${cspLinks.cboardDownload};
-    img-src * data: filesystem: blob: ;
-    media-src * data: filesystem: blob: ;
-    form-action 'self' ${cspType.unsafeEval} ${cspType.unsafeInline} ${
-    cspLinks.app
-  } ${cspLinks.localHost};
-    frame-src 'self' ${cspLinks.app} ${cspLinks.paypal} ${
+  } ${cspLinks.googleTagManager} ${cspLinks.cboardDownload} ${
+    cspLinks.vsTrack
+  };`;
+
+  const imgSrc = `img-src * data: filesystem: blob: ${cspLinks.filePath} ${
+    cspLinks.cloudfront
+  };`;
+
+  const mediaSrc = `media-src * data: filesystem: blob: ${cspLinks.filePath};`;
+
+  const formAction = `form-action 'self' ${cspType.unsafeEval} ${
+    cspType.unsafeInline
+  } ${cspLinks.app} ${cspLinks.localHost};`;
+
+  const frameSrc = `frame-src 'self' ${cspLinks.app} ${cspLinks.paypal} ${
     cspLinks.paypalSandbox
   } ${cspLinks.localHost} ${cspLinks.googleLinks}
      ${cspLinks.doubleclick} ${cspLinks.googleTagManager} ${
     cspLinks.tdDoubleClick
-  } ${cspLinks.youtube};
-    font-src 'self' data: ${cspLinks.paypal} ${cspLinks.paypalSandbox} ${
-    cspLinks.localHost
-  };`
+  } ${cspLinks.youtube};`;
+
+  const fontSrc = `font-src 'self' data: ${cspLinks.paypal} ${
+    cspLinks.paypalSandbox
+  } ${cspLinks.localHost} ${cspLinks.filePath};`;
+
+  const baseUri = `base-uri 'self';`;
+
+  const cspContent = `
+    ${defaultSrc}
+    ${scriptSrc}
+    ${styleSrc}
+    ${connectedSrc}
+    ${imgSrc}
+    ${mediaSrc}
+    ${formAction}
+    ${frameSrc}
+    ${fontSrc}
+    ${baseUri}
+    `
     .replace(/\s+/g, ' ')
     .trim();
 
