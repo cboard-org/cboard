72e90aaa7af671fa1e521e97642f0a382b099a79
tomivm <tomivm98@gmail.com>
2025-06-10 17:54:57 +0200

    Refactor LostedFolderForLoadBoardAlert to use a more detailed type definition

--- src/components/Board/TileEditor/LostedFolderForLoadBoardAlert/LostedFolderForLoadBoardAlert.tsx
+++ src/components/Board/TileEditor/LostedFolderForLoadBoardAlert/LostedFolderForLoadBoardAlert.tsx
@@ -2,11 +2,17 @@ import * as React from 'react';
 import { Alert, AlertTitle } from '@material-ui/lab';
 import messages from '../TileEditor.messages';
 
-export function LostedFolderForLoadBoardAlert({
-  intl
-}: {
-  intl: { formatMessage: (message: string) => string };
-}) {
+export interface LostedFolderForLoadBoardAlertProps {
+  intl: {
+    formatMessage: (
+      message: { id: string; defaultMessage?: string; description?: string }
+    ) => string;
+  };
+}
+
+export const LostedFolderForLoadBoardAlert: React.FC<
+  LostedFolderForLoadBoardAlertProps
+> = ({ intl }) => {
   return (
     <Alert className="TileEditor__loadBoard_Alert" severity="warning">
       <AlertTitle>
@@ -15,4 +21,4 @@ export function LostedFolderForLoadBoardAlert({
       {intl.formatMessage(messages.loadBoardAlertDescription)}
     </Alert>
   );
-}
+};
