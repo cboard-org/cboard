82eb2326505b40f7cf81c996292ed75316946dbb
Jacob McGowan <jacobmcgowan@duck.com>
2025-11-11 04:39:39 -0500

    ARASAAC skin tone and hair color options (#1836)
    * Added skin tone and hair color select components.
    
    * Finished skin and hair color options.
    
    * Fixed issue with skin tone and hair color options not hiding.
    
    * Fixed failing tests.
    
    * Removed unused import.
    
    * Moved inclusivity options to the left on large displays.
    
    * Moved skin tone and hair color selections to autosuggest buttons.
    
    * Removed !important from CSS for ColorSelectDropdown.
    
    * Ensured that color select dropdown closes when a value is selected or clicked outside of.
    
    * Reverted some changes to fetchArasaacSuggestions.
    
    * Removed unused variable.
    
    * Removed unecessary change.

---
+++ src/components/UI/ColorSelect/HairColor.messages.js
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  hairColor: {
+    id: 'cboard.components.HairColor.hairColor',
+    defaultMessage: 'Hair Color'
+  },
+  clearSelection: {
+    id: 'cboard.components.HairColor.clearSelection',
+    defaultMessage: 'Clear selection'
+  }
+});
