e7fb7799b03c7882a63d36add871b3b0e8358565
Jacob McGowan <jacobmcgowan@duck.com>
2025-11-11 21:23:43 -0500

    Added tour for Symbol Search to describe skin tone and hair color options.

--- src/components/Board/SymbolSearch/SymbolSearch.messages.js
+++ src/components/Board/SymbolSearch/SymbolSearch.messages.js
@@ -12,5 +12,40 @@ export default defineMessages({
   uploadAnImage: {
     id: 'cboard.components.InputImage.uploadImage',
     defaultMessage: 'Upload an image'
+  },
+  walkthroughEndTour: {
+    id: 'cboard.components.Settings.walkthroughEndTour',
+    defaultMessage: 'End Tour'
+  },
+  walkthroughCloseTour: {
+    id: 'cboard.components.Settings.walkthroughCloseTour',
+    defaultMessage: 'Close Tour'
+  },
+  walkthroughBack: {
+    id: 'cboard.components.Settings.walkthroughBack',
+    defaultMessage: 'Back'
+  },
+  walkthroughNext: {
+    id: 'cboard.components.Settings.walkthroughNext',
+    defaultMessage: 'Next'
+  },
+  walkthroughSymbolSearch: {
+    id: 'cboard.components.SymbolSearch.walkthroughSymbolSearch',
+    defaultMessage: 'Welcome to the symbol search!'
+  },
+  walkthroughSymbolSearchDesc: {
+    id: 'cboard.components.SymbolSearch.walkthroughSymbolSearchDesc',
+    defaultMessage:
+      'The symbol search allows you to find symbols from various libraries to use for your board tiles.'
+  },
+
+  skinToneAndHairColorOptionsTitle: {
+    id: 'cboard.components.SymbolSearch.skinToneAndHairColorOptionsTitle',
+    defaultMessage: 'Skin tone and hair color options'
+  },
+  skinToneAndHairColorOptionsDesc: {
+    id: 'cboard.components.SymbolSearch.skinToneAndHairColorOptionsDesc',
+    defaultMessage:
+      'You can customize the skin tone and hair color of symbols to better represent yourself and others. These options currently only apply to ARASAAC symbols.'
   }
 });
---
+++ src/components/Board/SymbolSearch/SymbolSearchTour.component.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Joyride, { STATUS } from 'react-joyride';
+import messages from './SymbolSearch.messages';
+import { FormattedMessage, intlShape } from 'react-intl';
+import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper/core';
+import 'swiper/swiper.min.css';
+import 'swiper/components/navigation/navigation.min.css';
+import 'swiper/components/pagination/pagination.min.css';
+
+import './SymbolSearch.css';
+
+SwiperCore.use([Navigation, Pagination, Autoplay]);
+
+const propTypes = {
+  isSymbolSearchTourEnabled: PropTypes.bool.isRequired,
+  disableTour: PropTypes.func.isRequired,
+  intl: intlShape.isRequired
+};
+
+const joyRideStyles = {
+  options: {
+    arrowColor: '#eee',
+    backgroundColor: '#eee',
+    primaryColor: '#aa00ff',
+    textColor: '#333',
+    width: 500,
+    zIndex: 10000
+  },
+  tooltipContent: {
+    padding: '5px 5px'
+  }
+};
+
+function SymbolSearchTour({ intl, disableTour, isSymbolSearchTourEnabled }) {
+  const symbolSearchTourSteps = [
+    {
+      target: 'body',
+      placement: 'center',
+      hideCloseButton: true,
+      content: (
+        <div>
+          <h2>
+            <FormattedMessage {...messages.walkthroughSymbolSearch} />
+          </h2>
+          <h5>
+            <FormattedMessage {...messages.walkthroughSymbolSearchDesc} />
+          </h5>
+        </div>
+      )
+    },
+    {
+      target: '#HairColorOptions',
+      hideCloseButton: true,
+      content: (
+        <div>
+          <h2>
+            <FormattedMessage {...messages.skinToneAndHairColorOptionsTitle} />
+          </h2>
+          <h5>
+            <FormattedMessage {...messages.skinToneAndHairColorOptionsDesc} />
+          </h5>
+        </div>
+      )
+    }
+  ];
+
+  return (
+    <div>
+      <Joyride
+        callback={data => {
+          const { status } = data;
+          if ([STATUS.FINISHED, STATUS.SKIPPED].includes(status)) {
+            disableTour({ isSymbolSearchTourEnabled: false });
+          }
+        }}
+        steps={symbolSearchTourSteps}
+        continuous={true}
+        showSkipButton={true}
+        showProgress={false}
+        disableOverlayClose={true}
+        run={isSymbolSearchTourEnabled}
+        scrollOffset={500}
+        spotlightPadding={4}
+        styles={joyRideStyles}
+        scrollDuration={100}
+        disableBeacon={true}
+        locale={{
+          last: intl.formatMessage(messages.walkthroughEndTour),
+          skip: intl.formatMessage(messages.walkthroughCloseTour),
+          next: intl.formatMessage(messages.walkthroughNext),
+          back: intl.formatMessage(messages.walkthroughBack)
+        }}
+      />
+    </div>
+  );
+}
+SymbolSearchTour.propTypes = propTypes;
+
+export default SymbolSearchTour;
