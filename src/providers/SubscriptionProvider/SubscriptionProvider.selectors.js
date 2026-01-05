35132fab9c895701095e960cba1409b0502f4d10
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2026-01-05 18:18:19 -0300

    Refactor isSubscriptionRequired to early return if user is not logged in

--- src/providers/SubscriptionProvider/SubscriptionProvider.selectors.js
+++ src/providers/SubscriptionProvider/SubscriptionProvider.selectors.js
@@ -1,4 +1,10 @@
+import { isLogged } from '../../components/App/App.selectors';
+
 export const isSubscriptionRequired = state => {
+  if (!isLogged(state)) {
+    return false;
+  }
+
   const { isInFreeCountry, isSubscribed, isOnTrialPeriod } = state.subscription;
   return !isInFreeCountry && !isSubscribed && !isOnTrialPeriod;
 };
