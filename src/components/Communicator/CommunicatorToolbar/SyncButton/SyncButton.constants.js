24fa0be9da431e62549e8fd3de4fda7c81054c93
Agus <aguslasp@gmail.com>
2026-02-06 16:14:03 -0300

    Add SyncButton constants for synchronization states and display duration

---
+++ src/components/Communicator/CommunicatorToolbar/SyncButton/SyncButton.constants.js
@@ -0,0 +1,10 @@
+export const SYNCED_DISPLAY_DURATION = 3000;
+
+export const DISPLAY_STATE = {
+  OFFLINE: 'offline',
+  WORKING_OFFLINE: 'workingOffline',
+  SAVING: 'saving',
+  SYNCED: 'synced',
+  PENDING: 'pending',
+  SYNC: 'sync'
+};
