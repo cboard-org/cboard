e95b76c3e5ff9ca362259a2155b407fc0c8b6a66
Agus <aguslasp@gmail.com>
2026-02-06 16:14:11 -0300

    Add SyncButton container for state management and synchronization actions

---
+++ src/components/Communicator/CommunicatorToolbar/SyncButton/SyncButton.container.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import { injectIntl } from 'react-intl';
+import SyncButton from './SyncButton.component';
+import { hasPendingSyncBoards } from '../../../Board/Board.selectors';
+import { getApiMyBoards } from '../../../Board/Board.actions';
+
+const mapStateToProps = state => ({
+  isOnline: state.app.isConnected,
+  isSyncing: state.board.isSyncing,
+  isFetching: state.board.isFetching,
+  hasPendingBoards: hasPendingSyncBoards(state)
+});
+
+const mapDispatchToProps = {
+  onSyncClick: getApiMyBoards
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(injectIntl(SyncButton));
