b89a2a78c0a37903679d462f612b7bc4183b39a7
Agus <aguslasp@gmail.com>
2026-02-06 16:14:29 -0300

    Add SyncButton messages for internationalization support

---
+++ src/components/Communicator/CommunicatorToolbar/SyncButton/SyncButton.messages.js
@@ -0,0 +1,24 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  offline: {
+    id: 'cboard.components.CommunicatorToolbar.SyncButton.offline',
+    defaultMessage: 'Offline'
+  },
+  workingOffline: {
+    id: 'cboard.components.CommunicatorToolbar.SyncButton.workingOffline',
+    defaultMessage: 'Working Offline'
+  },
+  saving: {
+    id: 'cboard.components.CommunicatorToolbar.SyncButton.saving',
+    defaultMessage: 'Saving'
+  },
+  synced: {
+    id: 'cboard.components.CommunicatorToolbar.SyncButton.synced',
+    defaultMessage: 'Synced'
+  },
+  sync: {
+    id: 'cboard.components.CommunicatorToolbar.SyncButton.sync',
+    defaultMessage: 'Sync'
+  }
+});
