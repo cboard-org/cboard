04af3a3726daa45191fedd45743c9ea122b652ca
Agus <aguslasp@gmail.com>
2026-02-09 14:23:24 -0300

    Add isSaving prop to mapStateToProps in SyncButton container

--- src/components/Communicator/CommunicatorToolbar/SyncButton/SyncButton.container.js
+++ src/components/Communicator/CommunicatorToolbar/SyncButton/SyncButton.container.js
@@ -4,10 +4,11 @@ import SyncButton from './SyncButton.component';
 import { hasPendingSyncBoards } from '../../../Board/Board.selectors';
 import { getApiMyBoards } from '../../../Board/Board.actions';
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state, ownProps) => ({
   isOnline: state.app.isConnected,
   isSyncing: state.board.isSyncing,
   isFetching: state.board.isFetching,
+  isSaving: ownProps.isSaving || false,
   hasPendingBoards: hasPendingSyncBoards(state)
 });
 
