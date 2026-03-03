c171d5798568621ff29d0321b37cf6d88de42cc8
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2026-03-03 18:48:06 -0300

    Relocate  MARK_BOARD_DIRTY constant

--- src/components/Board/Board.constants.js
+++ src/components/Board/Board.constants.js
@@ -43,6 +43,8 @@ export const DOWNLOAD_IMAGE_FAILURE = 'cboard/Board/DOWNLOAD_IMAGE_FAILURE';
 export const SYNC_BOARDS_STARTED = 'cboard/Board/SYNC_BOARDS_STARTED';
 export const SYNC_BOARDS_SUCCESS = 'cboard/Board/SYNC_BOARDS_SUCCESS';
 export const SYNC_BOARDS_FAILURE = 'cboard/Board/SYNC_BOARDS_FAILURE';
+export const MARK_BOARD_DIRTY = 'cboard/Board/MARK_BOARD_DIRTY';
+
 export const DEFAULT_ROWS_NUMBER = 5;
 export const DEFAULT_COLUMNS_NUMBER = 5;
 export const SHORT_ID_MAX_LENGTH = 14;
@@ -51,5 +53,3 @@ export const SYNC_STATUS = {
   SYNCED: 'synced',
   PENDING: 'pending'
 };
-
-export const MARK_BOARD_DIRTY = 'cboard/Board/MARK_BOARD_DIRTY';
