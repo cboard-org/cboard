dfb999ea8507c5ff9c4eb736626b7caef59f9872
Kumari Vaishnavi <145796948+vaishnavijha12@users.noreply.github.com>
2026-05-14 16:33:35 +0530

    Add action creators for queue management

--- src/components/CommunicationQueue/CommunicationQueue.actions.js
+++ src/components/CommunicationQueue/CommunicationQueue.actions.js
@@ -1,6 +1,25 @@
-import { QUEUE_EVENT } from './CommunicationQueue.constants';
+import {
+  QUEUE_EVENT,
+  REMOVE_EVENTS,
+  INCREMENT_RETRY,
+  CLEAR_QUEUE
+} from './CommunicationQueue.constants';
 
 export const queueEvent = payload => ({
   type: QUEUE_EVENT,
   payload
+});
+
+export const removeEvents = eventIds => ({
+  type: REMOVE_EVENTS,
+  payload: eventIds
+});
+
+export const incrementRetry = eventId => ({
+  type: INCREMENT_RETRY,
+  payload: eventId
+});
+
+export const clearQueue = () => ({
+  type: CLEAR_QUEUE
 });
