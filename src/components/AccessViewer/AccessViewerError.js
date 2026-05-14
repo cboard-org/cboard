720a8426d48a2111186fa98dce8f76de9017d622
Agus <aguslasp@gmail.com>
2026-05-14 12:17:45 -0300

    Replaced error messages with translations

--- src/components/AccessViewer/AccessViewerError.js
+++ src/components/AccessViewer/AccessViewerError.js
@@ -1,44 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { injectIntl, intlShape } from 'react-intl';
 import ErrorIcon from '@material-ui/icons/Error';
 import BlockIcon from '@material-ui/icons/Block';
+import messages from './AccessViewer.messages';
 import './AccessViewer.css';
 
-const ERROR_MESSAGES = {
+const ERROR_CONFIG = {
   invalid: {
     Icon: ErrorIcon,
-    title: 'Invalid or expired access code',
-    description:
-      'The access code you are trying to use does not exist or has expired. Contact the establishment for more information.'
+    title: 'invalidTitle',
+    description: 'invalidDescription'
   },
   forbidden: {
     Icon: BlockIcon,
-    title: 'Access not allowed',
-    description: 'This board is not available with the provided code.'
+    title: 'forbiddenTitle',
+    description: 'forbiddenDescription'
   },
   error: {
     Icon: ErrorIcon,
-    title: 'Connection error',
-    description:
-      'We could not load the board. Please check your connection and try again.'
+    title: 'errorTitle',
+    description: 'errorDescription'
   }
 };
 
-const AccessViewerError = ({ type }) => {
-  const { Icon, title, description } =
-    ERROR_MESSAGES[type] || ERROR_MESSAGES.error;
+const AccessViewerError = ({ type, intl }) => {
+  const { Icon, title, description } = ERROR_CONFIG[type] || ERROR_CONFIG.error;
 
   return (
     <div className="AccessViewer__error">
       <Icon className="AccessViewer__error-icon" />
-      <h2>{title}</h2>
-      <p>{description}</p>
+      <h2>{intl.formatMessage(messages[title])}</h2>
+      <p>{intl.formatMessage(messages[description])}</p>
     </div>
   );
 };
 
 AccessViewerError.propTypes = {
-  type: PropTypes.oneOf(['invalid', 'forbidden', 'error']).isRequired
+  type: PropTypes.oneOf(['invalid', 'forbidden', 'error']).isRequired,
+  intl: intlShape.isRequired
 };
 
-export default AccessViewerError;
+export default injectIntl(AccessViewerError);
