99e4d0b74dcbda5ff0a86cd3fab5f9d019df807e
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2026-06-10 15:58:00 -0300

    fix(sync): update updateApiMarkedBoards to continue on missing board IDs

--- src/components/Board/Board.actions.js
+++ src/components/Board/Board.actions.js
@@ -1095,7 +1095,7 @@ export function updateApiMarkedBoards() {
     const allBoards = [...getState().board.boards];
     for await (const board of allBoards) {
       const boardsIds = getState().board.boards?.map(board => board.id);
-      if (!boardsIds.includes(board.id)) return;
+      if (!boardsIds.includes(board.id)) continue;
 
       if (
         isServerBoard(board) &&
