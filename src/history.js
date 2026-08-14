e9c0db157adac464517861c7a4ab578e2b525e5a
martin bedouret <martinbedouret@gmail.com>
2026-08-14 00:06:07 -0300

    style: fix prettier formatting in history.js

--- src/history.js
+++ src/history.js
@@ -4,7 +4,6 @@ import { createBrowserHistory, createHashHistory } from 'history';
 // their non-web scheme (Electron/Android file://, iOS app://localhost) and use
 // hash history there; genuine web (http/https) uses browser history.
 const isWebHosted =
-  window.location.protocol === 'http:' ||
-  window.location.protocol === 'https:';
+  window.location.protocol === 'http:' || window.location.protocol === 'https:';
 const history = isWebHosted ? createBrowserHistory() : createHashHistory();
 export default history;
