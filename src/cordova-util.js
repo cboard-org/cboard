e070d021e60e0832d595fd929ccd3b010c20a107
martin bedouret <martinbedouret@gmail.com>
2026-08-14 00:50:35 -0300

    style: satisfy prettier in cordova-util.js (isPackagedApp on one line)

--- src/cordova-util.js
+++ src/cordova-util.js
@@ -12,8 +12,7 @@ export const isIOS = () => isCordova() && window.cordova.platformId === 'ios';
 // be set first): packaged apps are served over a non-web scheme (Electron and
 // Android file://, iOS app://localhost).
 export const isPackagedApp = () =>
-  window.location.protocol !== 'http:' &&
-  window.location.protocol !== 'https:';
+  window.location.protocol !== 'http:' && window.location.protocol !== 'https:';
 
 export const onCordovaReady = (onReady) =>
   document.addEventListener('deviceready', onReady, false);
